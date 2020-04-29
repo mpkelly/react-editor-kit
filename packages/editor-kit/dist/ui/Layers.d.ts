538efadeda28161c2984637aa56346d73631924d
mkelly <kikemelly@gmail.com>
2020-04-29 16:46:57 +0700

    (release) build for 0.0.52

--- packages/editor-kit/dist/ui/Layers.d.ts
+++ packages/editor-kit/dist/ui/Layers.d.ts
@@ -3,4 +3,7 @@ export declare const EditorLayer = 1;
 export declare const FocusedLayer = 2;
 export declare const ResizeHandleLayer = 999;
 export declare const OverlayLayer = 1000;
+export declare const MenuLayer = 900;
+export declare const MenuItemLayer: number;
+export declare const HoverItemLayer: number;
 export declare const PopupContentLayer: number;
--- packages/editor-kit/dist/ui/Utils.d.ts
+++ packages/editor-kit/dist/ui/Utils.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="react" />
 export declare const isInside: (rect: ClientRect, x: number, y: number) => boolean;
 export declare const isDeleting: (event: import("react").KeyboardEvent<Element>) => boolean;
-export declare const stopEvent: (event: MouseEvent | import("react").KeyboardEvent<Element> | import("react").MouseEvent<any, MouseEvent>) => boolean;
-export declare const blockEvent: (event: MouseEvent | KeyboardEvent | import("react").KeyboardEvent<Element> | import("react").MouseEvent<any, MouseEvent>) => boolean;
+export declare const stopEvent: (event: import("react").KeyboardEvent<Element> | MouseEvent | import("react").MouseEvent<any, MouseEvent>) => boolean;
+export declare const blockEvent: (event: import("react").KeyboardEvent<Element> | MouseEvent | import("react").MouseEvent<any, MouseEvent> | KeyboardEvent) => boolean;
 export declare const noOp: (...args: any[]) => void;
 export declare const clone: (value: any) => any;
