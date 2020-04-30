38b9d5f88e0588673b48290ca3aed855ddd6e694
mkelly <kikemelly@gmail.com>
2020-04-30 11:05:06 +0700

    (fix) Make sure image button is disabled when image cannot be inserted

--- packages/editor-kit/src/features/menu/MenuButton.tsx
+++ packages/editor-kit/src/features/menu/MenuButton.tsx
@@ -2,6 +2,8 @@ import React, { Fragment, useState, useRef } from "react";
 import { MenuProps, Menu } from "./Menu";
 import { IconProps, IconButton } from "../buttons/IconButton";
 import { ModalPopup } from "../popup/HTMLElementModalPopup";
+import { useLastFocused } from "../../editor/LastFocusedNode";
+import { useEditorKit } from "../../editor/EditorKit";
 
 export interface MenuButtonProps
   extends IconProps,
@@ -9,10 +11,16 @@ export interface MenuButtonProps
 
 export const MenuButton = (props: MenuButtonProps) => {
   const { children, style, ...rest } = props;
+  const { editor } = useEditorKit();
   const [show, setShow] = useState(false);
   const element = useRef<HTMLElement | null>();
+  const { node } = useLastFocused(editor);
+  const enabled = editor.isNodeSupported("image", node);
+
   const toggleShow = () => {
-    setShow((show) => !show);
+    if (enabled) {
+      setShow((show) => !show);
+    }
   };
 
   return (
@@ -20,6 +28,7 @@ export const MenuButton = (props: MenuButtonProps) => {
       <IconButton
         {...rest}
         onMouseDown={toggleShow}
+        disabled={!enabled}
         onRef={(node) => (element.current = node)}
       ></IconButton>
       <ModalPopup
