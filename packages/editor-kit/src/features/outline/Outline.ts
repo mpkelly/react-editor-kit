03c132282405f65e0b4a39fb8925cabde48f8e8b
mkelly <kikemelly@gmail.com>
2020-05-24 19:49:32 +0700

    (feat) Outline: support depth property

--- packages/editor-kit/src/features/outline/Outline.ts
+++ packages/editor-kit/src/features/outline/Outline.ts
@@ -11,17 +11,25 @@ export interface OutlineEntry {
 export const createOutline = (
   nodes: Node[],
   types = DefaultOutlineTypes,
-  flat = true
+  flat = false
 ): OutlineEntry[] => {
   if (flat) {
     return nodes
       .filter((node) => types.includes(node.type))
-      .map((node) => ({ content: Node.string(node), depth: 0, node }));
+      .map((node) => ({
+        content: Node.string(node),
+        depth: types.indexOf(node.type),
+        node,
+      }));
   } else {
     let outline: OutlineEntry[] = [];
     nodes.forEach((node) => {
       if (types.includes(node.type)) {
-        outline.push({ content: Node.string(node), depth: 0, node });
+        outline.push({
+          content: Node.string(node),
+          depth: types.indexOf(node.type),
+          node,
+        });
       }
       if (node.children) {
         outline = outline.concat(createOutline(node.children, types, false));
